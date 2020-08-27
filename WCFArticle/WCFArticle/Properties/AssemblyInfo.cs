f4379361c817a6642ce2e489fcd70e169086c818
bojana-b <66784194+bojana-b@users.noreply.github.com>
2020-08-27 20:04:47 +0200

    Add WCFArticle

--- WCFArticle/WCFArticle/Properties/AssemblyInfo.cs
+++ WCFArticle/WCFArticle/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("FileService")]
+[assembly: AssemblyTitle("WCFArticle")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("FileService")]
+[assembly: AssemblyProduct("WCFArticle")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("fe909ce8-db28-4666-93b5-c2ef2a29e72e")]
+[assembly: Guid("c0f1eed2-db95-4fee-b6e8-baef8f66121d")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,7 +29,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
